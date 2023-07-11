abbfb47218f5ae98417dd989fe9504289286d6f3
Your name <you@example.com>
2023-07-11 13:11:54 -0700

    a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """Defines a Pascal's Triangle function."""
 
+
 def pascal_triangle(n):
     """Represent Pascal's Triangle of size n.
 
-  Returns a list of lists of integers representing the
-    Pascal’s triangle of n
+    Returns a list of lists of integers representing the triangle.
     """
     if n <= 0:
         return []
-    triangle = [[1]]
-    while len(triangle) != n:
-        hold = triangle[-1]
-        inside = [1]
-        for i in range(len(hold) - 1):
-            inside.append(hold[i] + hold[i + 1])
-        inside.append(1)
-        triangle.append(inside)
-    return triangle
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
